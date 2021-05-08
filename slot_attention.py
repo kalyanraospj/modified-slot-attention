5542e7349b860d35da780053ea7ddf205cc532e6
kalyanrao <kalyanraospj@gmail.com>
2021-05-08 14:14:20 +0530

    Add files via upload

---
+++ slot_attention.py
@@ -0,0 +1,63 @@
+from brc_pytorch.layers import BistableRecurrentCell, NeuromodulatedBistableRecurrentCell
+from torch import nn
+import torch
+
+class SlotAttention(nn.Module):
+    def __init__(self, num_slots, dim, iters=3, eps=1e-8, hidden_dim=128):
+        super().__init__()
+        self.num_slots = num_slots
+        self.iters = iters
+        self.eps = eps
+        self.scale = dim ** -0.5
+
+        self.slots_q = nn.Embedding(num_slots, dim)
+
+        self.to_q = nn.Linear(dim, dim)
+        self.to_k = nn.Linear(dim, dim)
+        self.to_v = nn.Linear(dim, dim)
+        #######################################################
+        instead of using GRU here we use BistableRecurrentCell
+        #######################################################
+        self.nbrnn = NeuromodulatedBistableRecurrentCell(dim, dim)
+        ########################################################
+        hidden_dim = max(dim, hidden_dim)
+
+        self.mlp = nn.Sequential(
+            nn.Linear(dim, hidden_dim),
+            nn.ReLU(inplace=True),
+            nn.Linear(hidden_dim, dim)
+        )
+
+        self.norm_input = nn.LayerNorm(dim)
+        self.norm_slots = nn.LayerNorm(dim)
+        self.norm_pre_ff = nn.LayerNorm(dim)
+
+    def forward(self, inputs):
+        b, n, d = inputs.shape
+        n_s = self.num_slots
+
+        slots = self.slots_q.weight.unsqueeze(0).repeat(b, 1, 1)
+
+        inputs = self.norm_input(inputs)
+        k, v = self.to_k(inputs), self.to_v(inputs)
+
+        for _ in range(self.iters):
+            slots_prev = slots
+
+            slots = self.norm_slots(slots)
+            q = self.to_q(slots)
+
+            dots = torch.einsum('bid,bjd->bij', q, k) * self.scale
+            attn = dots.softmax(dim=1) + self.eps
+            attn = attn / attn.sum(dim=-1, keepdim=True)
+
+            updates = torch.einsum('bjd,bij->bid', v, attn)
+
+            slots = self.nbrnn(
+                updates.reshape(-1, d),
+                slots_prev.reshape(-1, d)
+            )
+
+            slots = slots.reshape(b, -1, d)
+            slots = slots + self.mlp(self.norm_pre_ff(slots))
+        return slots
